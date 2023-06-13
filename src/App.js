95a2b4ff6d3ed0a45489e81ccd57cf11772c569e
Pankaj Bisen <pankajpkj7888@gmail.com>
2023-06-13 18:39:40 +0530

    Render Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -35,12 +41,20 @@ function App() {
   }
   return (
     <>
-      <NavBar title='TextUtils' mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />
-        <About />
-      </div>
+      <Router>
+        <NavBar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
